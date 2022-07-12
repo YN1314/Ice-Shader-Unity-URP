5eaf99b2dbeea97f4ce4782585e6fb5c8eeab9f8
Danila Polyakov <tojynick@protonmail.com>
2022-07-12 10:22:41 +0300

    Project restructuring for more convenience

--- Assets/Shaders/Library/HLSL/Ice Depth.hlsl
+++ Assets/Shaders/Library/HLSL/Ice Depth.hlsl
@@ -1,15 +1,19 @@
+/*
+    Basically, the shader samples the cracks multiple times (Samples parameter)
+    Each sample slightly changes the UV based on tangent space's view direction.
+    At the end the shader combines the cracks texture and combined color of all samples.
+
+    This technique is called Parallax Mapping, you can read more about it here:
+    https://en.wikipedia.org/wiki/Parallax_mapping
+
+    Code is based on a tutorial by Jettelly https://www.youtube.com/watch?v=_dZthyScKF8
+*/
+
+
 // Unnecessary includes, used only for autocompletion
 #ifndef SHADERGRAPH_PREVIEW
     #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
 #endif
-
-
-/*
-    Basically, the shader samples a cracks texture multiple times (Samples parameter)
-    Each sample slightly changes the UV based on tangent space's view direction
-    
-    At the end the shader combines the cracks texture and combined color of all samples
-*/
 
 void CalculateIceDepth_half(
     in UnityTexture2D MainTexture,
